81dc68468d7d2eccaeca67b1a19bb4c3b93b3eeb
epson121 <traxdater12@gmail.com>
2013-12-03 17:34:45 +0100

    Added openbox config files and script for enabling the right click on touchpad

---
+++ rightClick.sh
@@ -0,0 +1,7 @@
+synclient RightButtonAreaLeft=1496 &
+synclient RightButtonAreaRight=3300 &
+synclient RightButtonAreaTop=1500 &
+synclient RightButtonAreaBottom=2300 &
+synclient AreaTopEdge=700 &
+synclient VertTwoFingerScroll=0 &
+
